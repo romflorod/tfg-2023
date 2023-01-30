9be1382895fc131ea2923f02a72891a5f5e9ba17
Roman Flores <64142185+romflorod@users.noreply.github.com>
2023-01-30 18:01:48 +0100

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,51 @@
+class Tournament:
+    def __init__(self, name, date, location, teams):
+        self.name = name
+        self.date = date
+        self.location = location
+        self.teams = teams
+
+class Team:
+    def __init__(self, name, players):
+        self.name = name
+        self.players = players
+
+class Player:
+    def __init__(self, name, number, position):
+        self.name = name
+        self.number = number
+        self.position = position
+
+class Game:
+    def __init__(self, home_team, away_team, date, location):
+        self.home_team = home_team
+        self.away_team = away_team
+        self.date = date
+        self.location = location
+
+def create_tournament(name, date, location, teams):
+    # Crea un nuevo torneo con los datos proporcionados y lo agrega a la base de datos
+
+def add_team_to_tournament(tournament, team):
+    # Agrega un equipo a un torneo existente en la base de datos
+
+def create_team(name, players):
+    # Crea un nuevo equipo con los datos proporcionados y lo agrega a la base de datos
+
+def add_player_to_team(team, player):
+    # Agrega un jugador a un equipo existente en la base de datos
+
+def create_player(name, number, position):
+    # Crea un nuevo jugador con los datos proporcionados y lo agrega a la base de datos
+
+def schedule_game(tournament, home_team, away_team, date, location):
+    # Programa un partido para un torneo existente en la base de datos
+
+def update_game_score(game, home_score, away_score):
+    # Actualiza el marcador de un partido en la base de datos
+
+def get_tournament_standings(tournament):
+    # Obtiene el ranking de los equipos en un torneo determinado
+
+def get_tournament_schedule(tournament):
+    # Obtiene el calendario de partidos de un torneo determinado
